4dc4607c7df6966663709458f881ef13fb9a051a
thomasaugustinov <augustinovthomas@gmail.com>
2023-11-13 01:03:52 +0200

    Saptamana 7: Fișiere. Directoare

--- README.md
+++ README.md
@@ -1,36 +1,42 @@
-# Program de Statistici pentru Fișiere BMP
+# Program de Statistici pentru Directoare, Fișiere BMP și Fișiere obișnuite
 
 ## Prezentare Generală
-Acest program în limbaj C prelucrează un fișier de intrare care reprezintă o imagine în format BMP pentru a genera un set de statistici. Programul este executat cu un singur argument după cum urmează:
 
-./program <fisier_intrare>
+Acest program în limbaj C este destinat parcurgerii unui director și generării unui set de statistici pentru fișierele conținute, cu manipulare specială pentru fișierele în format BMP. Programul este executat cu un singur argument care reprezintă calea către director:
+
+./program <director_intrare>
 
 
 ## Cerințe
-- Programul necesită exact un argument pentru a rula. Dacă acesta nu este furnizat, se afișează un mesaj de eroare: "Utilizare ./program <fisier_intrare>".
-- Fișierul de intrare trebuie să fie o imagine BMP. Programul verifică extensia fișierului și iese cu o eroare dacă extensia nu este `.bmp`.
+
+- Programul necesită exact un argument pentru a rula. Dacă acesta nu este furnizat, se afișează un mesaj de eroare: "Utilizare: ./program <director_intrare>".
+- Argumentul trebuie să fie calea către un director existent pe sistemul de fișiere.
 
 ## Funcționalități
-- Programul citește antetul fișierului BMP pentru a extrage lățimea și înălțimea imaginii.
-- Generează un fișier denumit `statistica.txt`, care include următoarele detalii:
-  - Numele fișierului
-  - Înălțimea imaginii
-  - Lățimea imaginii
-  - Dimensiunea fișierului în octeți
-  - ID-ul utilizatorului proprietar
-  - Timpul ultimei modificări
-  - Numărul de legături către fișier
-  - Drepturi de acces pentru utilizator, grup și alții
+
+- Programul parcurge recursiv directorul specificat ca argument.
+- Generează un fișier `statistica.txt` în directorul curent, care include detalii despre fiecare element din directorul de intrare:
+  - Pentru fișierele BMP: nume, înălțime, lățime, dimensiune, ID-ul utilizatorului, timpul ultimei modificări, numărul de legături, drepturi de acces pentru utilizator, grup și alții.
+  - Pentru fișierele obișnuite non-BMP: aceleași informații, excluzând înălțimea și lățimea.
+  - Pentru legăturile simbolice: nume legătură, dimensiune legătură, dimensiune fișier target, drepturi de acces.
+  - Pentru directoare: nume director, identificatorul utilizatorului, drepturi de acces.
+  - Pentru alte tipuri de fișiere, nu se scrie nimic în fișierul `statistica.txt`.
 
 ## Detalii Implementare
-- `FileExtension`: Validează calea fișierului și asigură că argumentul este un fișier cu o extensie.
-- `WriteFormattedToFile`: Scrie șiruri de caractere formatate într-un descriptor de fișier, gestionând erorile și închizând fișierul dacă scrierea eșuează.
-- `WritePermissionToFile`: Scrie informațiile despre permisiunile fișierului într-un format ușor de citit.
-- `SafeRead`: Citește în siguranță date din fișier, asigurându-se de gestionarea corespunzătoare a erorilor și închiderea fișierului în caz de eșec al citirii.
-- `main`: Punctul de intrare al programului care orchestrează citirea din fișierul BMP, extragerea statisticilor și scrierea în fișierul `statistica.txt`.
+
+- `FileExtension`: Identifică extensia unui fișier din calea sa.
+- `WriteFormattedToFile`: Scrie text formatat într-un fișier, tratând cazurile de eroare.
+- `WritePermissionToFile`: Scrie permisiunile unui fișier într-un mod lizibil.
+- `SafeRead`: Asigură citirea sigură dintr-un fișier, tratând cazurile de eroare.
 
 ## Gestionarea Erorilor
-Programul include o gestionare robustă a erorilor pentru operațiunile cu fișiere. Dacă orice apel de sistem eșuează, programul afișează un mesaj de eroare relevant, eliberează resursele și iese pentru a evita comportamentul nedefinit sau scurgerile de resurse.
+
+- Programul gestionează erorile întâmpinate în timpul execuției, afișând mesaje relevante și terminând execuția în mod corespunzător pentru a evita corupția de date sau scurgerile de resurse.
 
 ## Închiderea Resurselor
-La finalizare sau în caz de eroare, programul asigură că toate descriptoarele de fișiere sunt închise corespunzător pentru a preveni scurgerile de resurse.
+
+- Închiderea resurselor este realizată corespunzător pentru a preveni scurgerile de resurse, indiferent de modul în care programul se termină.
+
+
+
+
--- project.c
+++ project.c
@@ -7,6 +7,8 @@
 #include <fcntl.h>
 #include <time.h>
 #include <stdarg.h>
+#include <dirent.h>
+#include <limits.h>
 
 const char *FileExtension(const char path[]) {
     const char *result;
@@ -65,64 +67,107 @@ ssize_t SafeRead(int fileDescriptor, void *buffer, size_t count) {
 }
 
 int main(int argc, char **argv) {
-  if(argc < 2) {
-    printf("Not enough arguments\n");
-    exit(1);
-  }
-  char *fileName = argv[1];
-  int inputFd = open(fileName, O_RDONLY);
-  if (inputFd == -1) {
-    perror("Failed to open input file");
-    exit(1);
-  }
-  struct stat fileStats;
-  if (fstat(inputFd, &fileStats) == -1) {
-    perror("Failed to get file stats!\n");
-    close(inputFd);
-    exit(1);
-  }
-  const char *fileExtension = FileExtension(fileName);
-  if(strcmp(fileExtension, ".bmp") != 0) {
-      printf("File must have a BMP extension!\n");
-      exit(1);
+  if (argc != 2) {
+    fprintf(stderr, "Usage: %s <input_directory>\n", argv[0]);
+    exit(EXIT_FAILURE);
   }
   
+  DIR *dir;
+  struct dirent *entry;
+  struct stat entry_stat;
+  int outputFd;
+  char path[PATH_MAX];
   char outputFileName[] = "statistica.txt";
-  int outputFd = open(outputFileName, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IXUSR);
+  char unusedBuffer[1024];
+  
+  dir = opendir(argv[1]);
+  if (dir == NULL) {
+    perror("Failed to open directory");
+    exit(EXIT_FAILURE);
+  }
+  
+  outputFd = open(outputFileName, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
   if (outputFd == -1) {
     perror("Failed to open output file");
-    close(inputFd);
-    exit(1);
+    exit(EXIT_FAILURE);
   }
 
-  WriteFormattedToFile(outputFd, "nume fisier: %s\n", fileName);
+  while ((entry = readdir(dir)) != NULL) {
+    if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) {
+      continue;
+    }
+    
+    snprintf(path, PATH_MAX, "%s/%s", argv[1], entry->d_name);
+    if (lstat(path, &entry_stat) == -1) {
+      perror("Failed to get entry stats");
+      continue;
+    }
+    
+    if (S_ISREG(entry_stat.st_mode)) {
+      const char *fileExt = FileExtension(entry->d_name);
+      WriteFormattedToFile(outputFd, "nume fisier: %s\n", entry->d_name);
+      WriteFormattedToFile(outputFd, "dimensiune: %ld\n", (long)entry_stat.st_size);
+      WriteFormattedToFile(outputFd, "identificatorul utilizatorului: %d\n", entry_stat.st_uid);
+      WriteFormattedToFile(outputFd, "timpul ultimei modificari: %s", ctime(&entry_stat.st_mtime));
+      WriteFormattedToFile(outputFd, "contorul de legaturi: %ld\n", (long)entry_stat.st_nlink);
+      WritePermissionToFile(outputFd, entry_stat.st_mode, S_IRUSR, S_IWUSR, S_IXUSR, "drepturi de acces user: ");
+      WritePermissionToFile(outputFd, entry_stat.st_mode, S_IRGRP, S_IWGRP, S_IXGRP, "drepturi de acces grup: ");
+      WritePermissionToFile(outputFd, entry_stat.st_mode, S_IROTH, S_IWOTH, S_IXOTH, "drepturi de acces altii: ");
+      
+      if (fileExt != NULL && strcmp(fileExt, ".bmp") == 0) {
+	int width, height;
+	int bmpFileFd = open(path, O_RDONLY);
+	if (bmpFileFd == -1) {
+	  perror("Failed to open BMP file");
+	  continue;
+	}
+	SafeRead(bmpFileFd, unusedBuffer, 18);
+	SafeRead(bmpFileFd, &width, 4);
+	SafeRead(bmpFileFd, &height, 4);
+	WriteFormattedToFile(outputFd, "inaltime: %d\n", height);
+	WriteFormattedToFile(outputFd, "lungime: %d\n", width);
+	close(bmpFileFd);
+      }
+    }
+    else if (S_ISLNK(entry_stat.st_mode)) {
+      char targetPath[PATH_MAX];
+      ssize_t len = readlink(path, targetPath, sizeof(targetPath) - 1);
+      if (len == -1) {
+        perror("Failed to read symbolic link target");
+        continue;
+      }
+      targetPath[len] = '\0';
+      
+      struct stat targetStat;
+      if (stat(targetPath, &targetStat) == -1) {
+        perror("Failed to stat symbolic link target");
+        continue;
+      }
+      
+      WriteFormattedToFile(outputFd, "nume legatura: %s\n", entry->d_name);
+      WriteFormattedToFile(outputFd, "cale legatura: %s\n", targetPath);
+      WriteFormattedToFile(outputFd, "dimensiune legatura: %ld\n", (long)entry_stat.st_size);
+      WriteFormattedToFile(outputFd, "dimensiune fisier: %ld\n", (long)targetStat.st_size);
+      WritePermissionToFile(outputFd, targetStat.st_mode, S_IRUSR, S_IWUSR, S_IXUSR, "drepturi de acces user legatura: ");
+      WritePermissionToFile(outputFd, targetStat.st_mode, S_IRGRP, S_IWGRP, S_IXGRP, "drepturi de acces grup legatura: ");
+      WritePermissionToFile(outputFd, targetStat.st_mode, S_IROTH, S_IWOTH, S_IXOTH, "drepturi de acces altii legatura: ");
+    }
+    else if (S_ISDIR(entry_stat.st_mode)) {
+      WriteFormattedToFile(outputFd, "nume director: %s\n", entry->d_name);
+      WriteFormattedToFile(outputFd, "identificatorul utilizatorului: %d\n", entry_stat.st_uid);
+      WritePermissionToFile(outputFd, entry_stat.st_mode, S_IRUSR, S_IWUSR, S_IXUSR, "drepturi de acces user: ");
+      WritePermissionToFile(outputFd, entry_stat.st_mode, S_IRGRP, S_IWGRP, S_IXGRP, "drepturi de acces grup: ");
+      WritePermissionToFile(outputFd, entry_stat.st_mode, S_IROTH, S_IWOTH, S_IXOTH, "drepturi de acces altii: ");
+    }
+  }
 
-  char unusedBuffer[1024];
-  int width, height;
-
-  SafeRead(inputFd, unusedBuffer, 18);
-  SafeRead(inputFd, &width, 4);
-  SafeRead(inputFd, &height, 4);
-  
-  WriteFormattedToFile(outputFd, "inaltime: %d\n", height);
-  WriteFormattedToFile(outputFd, "lungime: %d\n", width);
-  WriteFormattedToFile(outputFd, "dimensiune: %ld\n", (long)fileStats.st_size);
-  WriteFormattedToFile(outputFd, "identificatorul utilizatorului: %d\n", fileStats.st_uid);
-  WriteFormattedToFile(outputFd, "timpul ultimei modificari: %s", ctime(&fileStats.st_mtime));
-  WriteFormattedToFile(outputFd, "contorul de legaturi: %ld\n", (long)fileStats.st_nlink);
-  
-  WritePermissionToFile(outputFd, fileStats.st_mode, S_IRUSR, S_IWUSR, S_IXUSR, "drepturi de acces user: ");
-  WritePermissionToFile(outputFd, fileStats.st_mode, S_IRGRP, S_IWGRP, S_IXGRP, "drepturi de acces grup: ");
-  WritePermissionToFile(outputFd, fileStats.st_mode, S_IROTH, S_IWOTH, S_IXOTH, "drepturi de acces altii: ");
-
-  if (close(inputFd) == -1) {
-    perror("Error closing input file");
-    exit(1);
+  if (closedir(dir) == -1) {
+    perror("Error closing directory");
   }
   
   if (close(outputFd) == -1) {
     perror("Error closing output file");
-    exit(1);
+    exit(EXIT_FAILURE);
   }
   
   return 0;
